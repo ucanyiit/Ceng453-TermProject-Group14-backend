b85ed7b230dc587184d8ba0b4481263704c21b13
Yiğitcan Uçan <ucanyiit@gmail.com>
2022-04-09 18:10:40 +0300

    feature: Set up mysql database connection with initial User model

--- src/main/java/com/example/backend/api/HelloWorldController.java
+++ src/main/java/com/example/backend/api/HelloWorldController.java
@@ -1,14 +1,31 @@
 package com.example.backend.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.backend.models.User;
+import com.example.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/")
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody
+    String addNewUser (@RequestParam String username
+            , @RequestParam String email) {
+
+        User n = new User();
+        n.setUsername(username);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
     }
 }
