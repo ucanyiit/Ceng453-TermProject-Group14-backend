d30c7192fc944b29be1dd9179f0bb6dbd5535f0b
bahadirkisbet <bahadirkisbet@gmail.com>
2022-04-09 18:17:44 +0300

    A controller for auth endpoint

---
+++ src/main/java/ceng453/backend/models/BaseResponse.java
@@ -0,0 +1,50 @@
+package ceng453.backend.models;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseResponse {
+    public boolean status;
+    public String message;
+    public String response;
+
+    public BaseResponse(boolean status, String message, String response) {
+        this.status = status;
+        this.message = message;
+        this.response = response;
+    }
+
+    public boolean getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getResponse() {
+        return this.response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public ResponseEntity<Object> prepareResponse(HttpStatus status) {
+        return new ResponseEntity<>(this, status);
+    }
+
+}
+
