ae600c98b9fa61df178f053c435f3d5f7e51807c
bahadirkisbet <bahadirkisbet@gmail.com>
2022-04-19 19:50:56 +0300

    FEATURE: a handler for auth

--- src/main/java/ceng453/backend/services/auth/AuthenticationService.java
+++ src/main/java/ceng453/backend/services/auth/AuthenticationService.java
@@ -1,11 +1,20 @@
 package ceng453.backend.services.auth;
 
 import ceng453.backend.models.BaseResponse;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthenticationService implements IAuthenticationService {
@@ -20,8 +29,29 @@ public class AuthenticationService implements IAuthenticationService {
      */
     @Override
     public ResponseEntity<BaseResponse> login(String username, String password) {
+        String token = createToken(username);
+        return new BaseResponse(true, "" , token).prepareResponse(HttpStatus.OK);
+    }
 
-        return null;
+    private String createToken(String username) {
+        String secretKey = "mySecretKey";
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                .commaSeparatedStringToAuthorityList("ROLE_USER");
+
+        String token = Jwts
+                .builder()
+                .setId("softtekJWT")
+                .setSubject(username)
+                .claim("authorities",
+                        grantedAuthorities.stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .collect(Collectors.toList()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 600_000))
+                .signWith(SignatureAlgorithm.HS512,
+                        secretKey.getBytes()).compact();
+
+        return "Bearer " + token;
     }
 
     @Override
