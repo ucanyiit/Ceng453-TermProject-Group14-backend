cae879fd03cc01f227de48728756bac5dbcae2ca
bahadirkisbet <bahadirkisbet@gmail.com>
2022-04-20 02:39:45 +0300

    CHANGE: Security options have changed.

--- src/main/java/ceng453/backend/BackendApplication.java
+++ src/main/java/ceng453/backend/BackendApplication.java
@@ -3,10 +3,10 @@ package ceng453.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import ceng453.backend.services.security.SecurityService;
@@ -24,12 +24,20 @@ public class BackendApplication {
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable()
-                    .addFilterAfter(new SecurityService(), UsernamePasswordAuthenticationFilter.class)
+            http
                     .authorizeRequests()
-                    .antMatchers(HttpMethod.POST, "/auth").permitAll()
-                    .antMatchers(HttpMethod.GET, "/").permitAll()
-                    .anyRequest().authenticated();
+                    .antMatchers("/api/auth/*").permitAll()
+                    .anyRequest()
+                    .fullyAuthenticated()
+                    .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .addFilterBefore(new SecurityService(), UsernamePasswordAuthenticationFilter.class)
+                    .formLogin().disable()
+                    .logout().disable()
+                    .httpBasic().disable()
+                    .csrf().disable();
         }
     }
 
