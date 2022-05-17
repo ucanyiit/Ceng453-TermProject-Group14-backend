2ff62a9bb97ea0b3bc9e566fd560a858778a85bf
bahadirkisbet <bahadirkisbet@gmail.com>
2022-05-17 03:48:00 +0300

    Add a new endpoint for rolling a dice during the game

--- src/main/java/ceng453/backend/api/game/GameController.java
+++ src/main/java/ceng453/backend/api/game/GameController.java
@@ -1,14 +1,12 @@
 package ceng453.backend.api.game;
 
+import ceng453.backend.models.DTOs.game.DiceDTO;
 import ceng453.backend.models.DTOs.game.StartGameDTO;
 import ceng453.backend.services.game.IGameService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/game")
 @RequiredArgsConstructor
@@ -29,8 +27,22 @@ public class GameController {
             notes = "Create a game with the given parameters",
             response = ResponseEntity.class
     )
-    @PostMapping(produces = "application/json")
+    @PostMapping(path = "/create-game", produces = "application/json")
     public Object createGame(@RequestBody StartGameDTO startGameDTO) {
         return gameService.createGame(startGameDTO.getType(), "testUsername", 2);
+    }
+
+    /**
+     * This method used by the players to roll a dice during a game
+     * @return A json object with the value of dice
+     */
+    @ApiOperation(
+            value = "Roll a dice during a game",
+            notes = "It is used to provide a dice functionality in a monopoly game",
+            response = ResponseEntity.class
+    )
+    @GetMapping(path = "/roll-dice", produces = "application/json")
+    public Object rollDice(@RequestParam int gameId, @RequestParam String token) {
+        return gameService.rollDice(gameId, token);
     }
 }
