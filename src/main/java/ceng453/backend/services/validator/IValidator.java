f4b1cbee2012f5adf3850bbaeb508742a628d1df
bahadirkisbet <bahadirkisbet@gmail.com>
2022-05-22 14:51:00 +0300

    feature: add a func to return valid actions of a player

--- src/main/java/ceng453/backend/services/validator/IValidator.java
+++ src/main/java/ceng453/backend/services/validator/IValidator.java
@@ -1,7 +1,13 @@
 package ceng453.backend.services.validator;
 
+import ceng453.backend.models.Action;
 import ceng453.backend.models.database.Game;
+import ceng453.backend.models.database.Player;
+
+import java.util.List;
 
 public interface IValidator {
     boolean isPlayersTurn(Game game, String username);
+
+    List<Action> getValidActions(Player player);
 }
