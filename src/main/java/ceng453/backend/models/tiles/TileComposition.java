a399491d9cc9bdce2888bbcf047f54151e4dfad2
bahadirkisbet <bahadirkisbet@gmail.com>
2022-05-22 14:46:39 +0300

    feature: extend the modularity of tile class

--- src/main/java/ceng453/backend/models/tiles/TileComposition.java
+++ src/main/java/ceng453/backend/models/tiles/TileComposition.java
@@ -7,16 +7,22 @@ import ceng453.backend.models.database.Property;
 import ceng453.backend.models.database.Tile;
 import ceng453.backend.models.enums.ActionType;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Getter
+@RequiredArgsConstructor
 public class TileComposition {
     protected Tile tile;
 
     public TileComposition(Property property, Game game, Player owner, Integer location, Integer price) {
         this.tile = new Tile(property, game, owner, location, price);
+    }
+
+    public Tile getTile() {
+        return tile;
     }
 
     public List<Action> onLand(Player player) {
--- src/main/java/ceng453/backend/repositories/TileRepository.java
+++ src/main/java/ceng453/backend/repositories/TileRepository.java
@@ -1,8 +1,9 @@
 package ceng453.backend.repositories;
 
+import ceng453.backend.models.database.Player;
 import ceng453.backend.models.database.Tile;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TileRepository extends CrudRepository<Tile, Integer> {
-
+    Tile findByPlayer(Player player);
 }
