e845f0c40b112f2a07833c9c4c878312434bb0a0
bahadirkisbet <bahadirkisbet@gmail.com>
2022-05-22 16:58:50 +0300

    fix: tile found problem solved by changing signature and query parameter of CRUD

--- src/main/java/ceng453/backend/repositories/TileRepository.java
+++ src/main/java/ceng453/backend/repositories/TileRepository.java
@@ -1,9 +1,10 @@
 package ceng453.backend.repositories;
 
+import ceng453.backend.models.database.Game;
 import ceng453.backend.models.database.Player;
 import ceng453.backend.models.database.Tile;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TileRepository extends CrudRepository<Tile, Integer> {
-    Tile findByPlayer(Player player);
+    Tile findByLocationAndGame(Integer location, Game game);
 }
