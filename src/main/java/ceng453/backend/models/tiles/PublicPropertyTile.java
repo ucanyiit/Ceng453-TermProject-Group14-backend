dc87f04c4b9540fa7ac6b5055e0417b2dd65bcd4
Yiğitcan Uçan <ucanyiit@gmail.com>
2022-06-21 15:23:40 +0300

    feature: Add double payment on public properties if owner has both of them

--- src/main/java/ceng453/backend/models/tiles/PublicPropertyTile.java
+++ src/main/java/ceng453/backend/models/tiles/PublicPropertyTile.java
@@ -7,15 +7,31 @@ import ceng453.backend.models.actions.PayAction;
 import ceng453.backend.models.database.Game;
 import ceng453.backend.models.database.Player;
 import ceng453.backend.models.database.Property;
+import ceng453.backend.models.database.Tile;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PublicPropertyTile extends TileComposition {
     private static final Integer PRICE = 250;
+    private Property property;
 
     public PublicPropertyTile(Property property, Game game, Integer location) {
         super(property, game, null, location, PRICE);
+        this.property = property;
+    }
+
+    private double getRent() {
+        List<Tile> tiles = this.tile.getOwner().getProperties();
+        for (Tile tile : tiles) {
+            Property property = tile.getProperty();
+            if (property.getName().startsWith(this.property.getName().substring(0, 3))
+                    && !property.getName().equals(this.property.getName())) {
+                return this.tile.getPrice() / 5.;
+            }
+        }
+
+        return this.tile.getPrice() / 10.;
     }
 
     public List<Action> onLand(Player player) {
@@ -29,7 +45,7 @@ public class PublicPropertyTile extends TileComposition {
         } else if (this.tile.getOwner().getUser().getUsername().equals(player.getUser().getUsername())) {
             actions.add(new NoAction(player));
         } else {
-            actions.add(new PayAction(player, this.tile.getOwner(), this.tile.getPrice().doubleValue() / 10));
+            actions.add(new PayAction(player, this.tile.getOwner(), this.getRent()));
         }
 
         return actions;
